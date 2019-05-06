e61b6b252de742e322cf83132d291bf764b6e21c
UasaLine <cop@ngs.ru>
2019-05-06 19:08:14 +0700

    add TypeOfFurniture

---
+++ src/main/java/com/jds/model/TypeOfFurniture.java
@@ -0,0 +1,12 @@
+package com.jds.model;
+
+public enum TypeOfFurniture {
+    TOP_LOCK,
+    LOWER_LOCK,
+    HANDLE,
+    CLOSER,
+    PEEPHOLE,
+    NIGHT_LOCK,
+    THRESHOLD,
+    END_DOOR_LOCK
+}
