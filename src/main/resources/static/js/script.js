68f9211e6a16ca96c4a216ec8a57497cf507d804
UasaLine <cop@ngs.ru>
2019-05-15 17:37:34 +0700

    use JQuery 03

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,11 +1,84 @@
 jQuery('document').ready(function(){
 
-	jQuery('.logo_text').on('click',function(){
-		alert('АУЕТЬ');
-	});
+    var currentItem  = "";
 
-    jQuery('.logo_text').on('click',function(){
-        alert('АУЕТЬ');
+
+    $('.vertical_menu_button').on('click',function(){
+
+        currentItem = $(this).attr('id');
+
+
+        if (currentItem == "doorclass")
+            $('.select_door_class').attr('id','is_alive_lement');
+        else
+            $('.select_door_class').attr('id','ghost_lement');
+
+        if (currentItem == "metal")
+            $('.select_metal').attr('id','is_alive_lement');
+        else
+            $('.select_metal').attr('id','ghost_lement');
+
+        if (currentItem == "widthDoor")
+            $('.select_widthDoor').attr('id','is_alive_lement');
+        else
+            $('.select_widthDoor').attr('id','ghost_lement');
     });
+
+
+    $('.images_div').on('click',function(){
+
+        set($(this).children('.images_door_class').attr('id'));
+        pickOut(this);
+
+    });
+
+
+    $('.ios-toggle').on('click',function(){
+
+        if ($(this).is(':checked')){
+
+            var elems = $('.ios-toggle');
+            var elemsTotal = elems.length;
+
+            for(var i=0; i<elemsTotal; ++i){
+                if ($(this).attr('id')==$(elems[i]).attr('id')){
+
+                }
+                else{
+                    $(elems[i]).prop('checked', false);
+                }
+            }
+            set($(this).attr('data'));
+        }
+        else{
+            set('');
+        }
+
+
+    });
+
+    //--------------------------------------
+    //seter
+    //--------------------------------------
+
+    function pickOut(item){
+
+        var elems = $('.images_div');
+        var elemsTotal = elems.length;
+        for(var i=0; i<elemsTotal; ++i){
+            $(elems[i]).attr('id', 'no')
+        }
+
+        $(item).attr('id','checkbox');
+    }
+
+
+    function set(Value){
+
+        $('.vertical_menu_button_rigtht#'+currentItem+' strong').html(Value);
+
+    }
+
+
 
 });
