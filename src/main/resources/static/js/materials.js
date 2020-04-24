23b4ca9711e8badee641602f73cfe106ce029a2d
UasaLine <yury.salagaev@gmail.com>
2020-04-24 10:15:35 +0700

    make a field change in a separate file

--- src/main/resources/static/js/materials.js
+++ src/main/resources/static/js/materials.js
@@ -1,6 +1,7 @@
+let changed = false;
 jQuery("document").ready(function () {
   var materialsList;
-  var changed = false;
+
   var currentLine = 0;
 
   grtListDoorClassToSelect();
@@ -30,19 +31,9 @@ jQuery("document").ready(function () {
     });
   });
 
-  $("tbody").on("dblclick", ".vary_field", function (e) {
-    var t = e.target || e.srcElement;
+  LineEditor.init();
 
-    var elm_name = t.tagName.toLowerCase();
-
-    if (elm_name == "input") {
-      return false;
-    }
-
-    addFieldInCell(this);
-
-    changed = true;
-
+  $("tbody").on("dblclick", function () {
     displayComponentVisibility();
   });
 
@@ -51,11 +42,7 @@ jQuery("document").ready(function () {
     oneEnableAllDisable(this);
   });
 
-  $(window).keydown(function (event) {
-    if (event.keyCode == 13) {
-      $("#edit").blur();
-    }
-  });
+
 
   $("#addLine").click(function () {
     if ($(this).hasClass("disabled")) {
@@ -116,69 +103,7 @@ jQuery("document").ready(function () {
     });
   });
 
-  function addFieldInCell(jsOb) {
-    if ($(jsOb).hasClass("text_select")) {
-      addSelect(jsOb);
-      $("#edit").focus();
-      $("#edit").blur(function () {
-        var val = $(this).val();
-        var name = getNameSelectedValue(val);
-        $(this).parent().empty().text(name).attr("val", val);
-      });
-    } else if ($(jsOb).hasClass("text_input")) {
-      addInput(jsOb);
-      $("#edit").focus();
-      $("#edit").blur(function () {
-        var val = $(this).val();
-        $(this).parent().empty().text(val);
-      });
-    }
-  }
 
-  function addSelect(jsOb) {
-    var val = $(jsOb).text();
-
-    $(jsOb).empty();
-    $("<select>")
-      .attr("class", "form-control")
-      .attr("id", "edit")
-      .appendTo(jsOb);
-
-    $("#edit").append($("<option></option>"));
-
-    if (availableValues != null) {
-      for (var i = 0; i < availableValues.length; i++) {
-        $("#edit").append(
-          $(
-            "<option value=" +
-              availableValues[i].id +
-              ">" +
-              availableValues[i].name +
-              "</option>"
-          )
-        );
-      }
-      setValueInSelect("#edit", val);
-    } else {
-      if (val != "") {
-        $("#edit").append($("<option>" + val + "</option>"));
-        setValueInSelect("#edit", val);
-      }
-    }
-  }
-
-  function addInput(jsOb) {
-    var val = $(jsOb).html();
-
-    $(jsOb).empty();
-    $("<input>")
-      .attr("class", "form-control")
-      .attr("type", "text")
-      .attr("id", "edit")
-      .appendTo(jsOb);
-
-    $("#edit").val(val);
-  }
 
   function grtListDoorClassToSelect() {
     $.ajax({
@@ -216,15 +141,6 @@ jQuery("document").ready(function () {
     return 0 + 1;
   }
 
-  function setValueInSelect(jqSelect, value) {
-    var opt = $(jqSelect + " > option");
-    opt.each(function (indx, element) {
-      if ($(this).text().toLowerCase() == value.toLowerCase()) {
-        $(this).attr("selected", "selected");
-      }
-    });
-  }
-
   function newInstansLineSpecification(id, materialId, name, value, formula) {
     var lim = new (function () {
       this.id = id;
@@ -235,34 +151,6 @@ jQuery("document").ready(function () {
       this.formula = formula;
     })();
     return lim;
-  }
-
-  function getMaterialIdByName(val) {
-    if (availableValues == null) {
-      return 0;
-    }
-
-    for (var i = 0; i < availableValues.length; i++) {
-      if (availableValues[i].name == val) {
-        return availableValues[i].id;
-      }
-    }
-
-    return 0;
-  }
-
-  function getNameSelectedValue(val) {
-    if (availableValues == null) {
-      return "";
-    }
-
-    for (var i = 0; i < availableValues.length; i++) {
-      if (availableValues[i].id == val) {
-        return availableValues[i].name;
-      }
-    }
-
-    return "";
   }
 
   function fillTabLine() {
