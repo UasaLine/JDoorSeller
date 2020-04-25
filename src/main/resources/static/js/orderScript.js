b7d1622465534682c4937da536d744d6b9b27b50
UasaLine <yury.salagaev@gmail.com>
2020-04-25 20:33:42 +0700

    count total

--- src/main/resources/static/js/orderScript.js
+++ src/main/resources/static/js/orderScript.js
@@ -137,14 +137,15 @@ jQuery("document").ready(function () {
         "кол-во</th><th>" +
         "металл</th><th>" +
         "цвет</th><th>" +
-        "цена</th></tr>"
+        "цена</th><th>"+
+        "сумма</th></tr>"
     );
 
     var doors = order.doors;
     var position = 1;
     for (var j = 0; j < doors.length; ++j) {
       $(".Table > tbody").append(
-        "<tr>" +
+        "<tr class='edit_line'>" +
           '<td class="position">' +
           position +
           "</td>" +
@@ -154,7 +155,7 @@ jQuery("document").ready(function () {
           "<td>" +
           doors[j].name +
           "</td>" +
-          '<td class="vary_field text_input" id='+doors[j].id+'>' +
+          '<td class="vary_field text_input quantity_field" id='+doors[j].id+'>' +
           doors[j].quantity +
           "</td>" +
           "<td>" +
@@ -163,8 +164,11 @@ jQuery("document").ready(function () {
           "<td>" +
           doors[j].doorColor +
           "</td>" +
-          "<td>" +
+          '<td class="price_line">' +
           doors[j].priceWithMarkup +
+          "</td>" +
+          '<td class="total_line">' +
+          (doors[j].priceWithMarkup * doors[j].quantity) +
           "</td>" +
           "</tr>"
       );
@@ -331,4 +335,15 @@ jQuery("document").ready(function () {
       }
     });
   }
+
+
 });
+
+function countTotalLine(){
+  $('tr').each(function () {
+    let quantity = $(this).children('.vary_field').text();
+    let price = $(this).children('.price_line').text();
+    let total = $(this).children('.total_line');
+    $(total).text(quantity*price);
+  })
+}
