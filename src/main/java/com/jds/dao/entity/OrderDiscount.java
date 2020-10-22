9f3118f46ea2076f5f3bad2be2a832d9125e0ace
mazed <mazeday54@gmail.com>
2020-10-22 19:13:37 +0700

    Door - 198 fix

--- src/main/java/com/jds/dao/entity/OrderDiscount.java
+++ src/main/java/com/jds/dao/entity/OrderDiscount.java
@@ -26,31 +26,13 @@ public class OrderDiscount {
     @Column(name = "discount")
     private int discount;
 
-//    @OneToOne(mappedBy = "order_door_discount",fetch = FetchType.LAZY)
-//    private DoorsОrder order;
-//
-//    @OneToOne(mappedBy = "order_door_discount",fetch = FetchType.LAZY)
-//    private DoorEntity door;
-
-    public OrderDiscount instanceOrderDoorDiscount(DoorsОrder doorsОrder, DoorEntity doorEntity) {
-        OrderDiscount orderDoorDiscount = new OrderDiscount();
-        orderDoorDiscount.setOrder_id(doorsОrder.getOrder_id());
-        orderDoorDiscount.setDoor_id(doorEntity.getId());
-
-        return orderDoorDiscount;
-    }
-
     public OrderDiscount clearNonSerializingFields() {
-//        order = null;
-//        door = null;
-
         if (order_id != 0){
             order_id = 0;
         }
         if (door_id != 0){
             door_id = 0;
         }
-
         return this;
     }
 }
