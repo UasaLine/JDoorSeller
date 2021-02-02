9311a4c7f4ad783fb4d239cb581d30c4de0a1fea
UasaLine <yury.salagaev@gmail.com>
2021-02-02 16:00:51 +0700

    refactoring

--- src/main/resources/static/js/doorTypeListScript.js
+++ src/main/resources/static/js/doorTypeListScript.js
@@ -1,67 +1,67 @@
 jQuery("document").ready(function () {
-  var typeId = 0;
-  var classId = $("#currentClassId").attr("value");
+    var typeId = 0;
+    var classId = $("#currentClassId").attr("value");
 
     changeListGroupItem(".list-group-item");
 
-  function changeListGroupItem(selector){
-    classId = $(selector).attr("data");
-    if (location.href == (location.origin + "/doortypelist") && classId != ""){
-      location.href = "doortypelist?classId=" + classId;
+    function changeListGroupItem(selector) {
+        classId = $(selector).attr("data");
+        if (location.href == (location.origin + "/pages/doortype") && classId != "") {
+            location.href = "doortype?classId=" + classId;
+        }
     }
-  }
 
-  $(".list-group").on("click", ".list-group-item", function () {
-    classId = $(this).attr("data");
-    location.href = "doortypelist?classId=" + classId;
-  });
-
-  $("tr").on("dblclick", function () {
-    getType($(this).children(".id").text());
-  });
-
-  $("tr").on("click", function () {
-    typeId = $(this).children(".id").text();
-    oneEnableAllDisable(this);
-  });
-
-  $("#buttonDelete").on("click", function () {
-    if (curreiId != 0) {
-      deletType();
-    } else {
-      alert("!выбери тип");
-    }
-  });
-
-  $("#addClass").on("click", function () {
-    getType(0);
-  });
-
-  function getType(typeId) {
-    location.href = "doortype?typeId=" + typeId + "&classId=" + classId;
-  }
-
-  function oneEnableAllDisable(item) {
-    var elems = $('tr[pickOut="on"]');
-    var elemsTotal = elems.length;
-
-    for (var i = 0; i < elemsTotal; ++i) {
-      $(elems[i]).attr("pickOut", "off");
-    }
-    $(item).attr("pickOut", "on");
-  }
-
-  function deletType() {
-    $.ajax({
-      type: "DELETE",
-      url: "doortype?typeId=" + typeId,
-      success: function (data) {
-        alert("delete completed" + data);
-        location.href = "doortypeList";
-      },
-      error: function (data) {
-        alert("delete error:" + data);
-      },
+    $(".list-group").on("click", ".list-group-item", function () {
+        classId = $(this).attr("data");
+        location.href = "doortype?classId=" + classId;
     });
-  }
+
+    $("tr").on("dblclick", function () {
+        getType($(this).children(".id").text());
+    });
+
+    $("tr").on("click", function () {
+        typeId = $(this).children(".id").text();
+        oneEnableAllDisable(this);
+    });
+
+    $("#buttonDelete").on("click", function () {
+        if (curreiId != 0) {
+            deletType();
+        } else {
+            alert("!выбери тип");
+        }
+    });
+
+    $("#addClass").on("click", function () {
+        getType(0);
+    });
+
+    function getType(typeId) {
+        location.href = "doortype/"+ typeId + "?classId=" + classId;
+    }
+
+    function oneEnableAllDisable(item) {
+        var elems = $('tr[pickOut="on"]');
+        var elemsTotal = elems.length;
+
+        for (var i = 0; i < elemsTotal; ++i) {
+            $(elems[i]).attr("pickOut", "off");
+        }
+        $(item).attr("pickOut", "on");
+    }
+
+    function deletType() {
+        $.ajax({
+            type: "DELETE",
+            url: "doortype?typeId=" + typeId,
+            success: function (data) {
+                alert("delete completed" + data);
+                location.href = "doortypeList";
+            },
+            error: function (data) {
+                alert("delete error:" + data);
+            },
+        });
+    }
 });
