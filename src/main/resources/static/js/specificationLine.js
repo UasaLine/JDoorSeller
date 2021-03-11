1e3d2e650c3812264d85499ca3b1b37fb1639f8c
UasaLine <yury.salagaev@gmail.com>
2021-03-11 12:59:36 +0700

    add select material and formula to line_specification

--- src/main/resources/static/js/specificationLine.js
+++ src/main/resources/static/js/specificationLine.js
@@ -4,8 +4,11 @@ jQuery("document").ready(function () {
     let specificationEtity;
     let id = $("#id").text();
     let line_id = $("#line_id").text();
+    let formulaList;
+    let materialList;
 
-    getFormulas();
+    let response = getFormulas();
+    response = getMaterials();
     getLineSpecification();
 
     $("#close").on("click", function () {
@@ -21,11 +24,12 @@ jQuery("document").ready(function () {
     });
 
     $("#formula").change(function () {
-        setField("formula", $("#formula").val());
+        setField("formula", fineInformulaList($("#formula").val()));
     });
 
-    $("#nameObject").change(function () {
-        setField("independentName", $("#nameObject").val());
+    $("#material").change(function () {
+        setFieldName($("#material").val());
+        setField("materialId", $("#material").val());
     });
 
     $("#releaseOperation").change(function () {
@@ -59,11 +63,30 @@ jQuery("document").ready(function () {
     }
 
     function getFormulas() {
-        $.ajax({
+        return $.ajax({
             url: location.origin + "/materials/formulas",
             dataType: "json",
+            async: false,
             success: function (data) {
+                formulaList = data;
                 fillFormulaSelect(data);
+                return data;
+            },
+            error: function (data) {
+                alert("!ERROR: данные о формулах получить не удалось:" + data);
+            },
+        });
+    }
+
+    function getMaterials() {
+        return $.ajax({
+            url: location.origin + "/materials",
+            dataType: "json",
+            async: false,
+            success: function (data) {
+                materialList = data;
+                fillMaterialSelect(data);
+                return data;
             },
             error: function (data) {
                 alert("!ERROR: данные о формулах получить не удалось:" + data);
@@ -113,15 +136,15 @@ jQuery("document").ready(function () {
     function fillInLineSpecification() {
         $("#name").val(lineSpecifications.name);
         $("#valueSpec").val(lineSpecifications.value);
-        setValueInSelect("#formula", lineSpecifications.formula);
-        $("#nameObject").val(lineSpecifications.independentName);
+        setValueInSelect("#formula", lineSpecifications.formula ? lineSpecifications.formula.id : null);
+        setValueInSelect("#material", lineSpecifications.materialId);
         $("#releaseOperation").val(lineSpecifications.releaseOperation);
         $("#writeOffOperation").val(lineSpecifications.writeOffOperation);
     }
 
     function setValueInSelect(jqSelect, value) {
-        var opt = $(jqSelect + " > option");
-        opt.each(function (indx, element) {
+        let option = $(jqSelect + " > option");
+        option.each(function (indx, element) {
             if ($(this).val() == value) {
                 $(this).attr("selected", "selected");
             }
@@ -138,12 +161,49 @@ jQuery("document").ready(function () {
 
             $("#formula").append($("<option></option>"));
 
-            for (var i = 0; i < list.length; ++i) {
+            list.forEach(function (item, i, arr) {
                 $("#formula").append(
-                    $("<option value=" + list[i].id + ">" + list[i].name + "</option>")
+                    $("<option value=" + item.id + ">" + item.name + "</option>")
                 );
-            }
+            });
         }
+    }
+
+    function fillMaterialSelect(list) {
+        if (list != null) {
+            $("#material").empty();
+
+            $("#material").append($("<option></option>"));
+
+            list.forEach(function (item, i, arr) {
+                $("#material").append(
+                    $("<option value=" + item.id + ">" + item.name + "</option>")
+                );
+            });
+        }
+    }
+
+    function fineInformulaList(id) {
+        return fineInListById(formulaList, id);
+    }
+
+    function fineInListById(list, id) {
+        const filterList = list.filter(function (item) {
+            return item.id == id;
+        });
+
+        if (filterList.length > 0) {
+            return filterList[0];
+        } else {
+            alert('the selected position was not found');
+            return null;
+        }
+    }
+
+    function setFieldName(id) {
+        const material = fineInListById(materialList, id);
+        setField("name", material.name);
+        $("#name").val(lineSpecifications.name);
     }
 });
 
