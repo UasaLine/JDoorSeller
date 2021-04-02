0b4ec9ef7f61230c700e4233dfbe52f214f01beb
Yury Salagaev <y.salagaev@12storeez.com>
2021-04-02 09:42:31 +0700

    fix_bag_pagination

--- src/main/resources/static/js/calc/calculation.script.js
+++ src/main/resources/static/js/calc/calculation.script.js
@@ -1698,7 +1698,10 @@ jQuery("document").ready(function () {
             'typeDoorGlass' == currentItem ||
             'toning' == currentItem ||
             'armor' == currentItem ||
-            'doorGlass' == currentItem
+            'doorGlass' == currentItem||
+            'outShieldColor' == currentItem ||
+            'shieldOverColor' == currentItem ||
+            'outShieldDesign' == currentItem
         ) {
             return true;
         }
