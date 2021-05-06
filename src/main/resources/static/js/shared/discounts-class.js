2f0fa6aa1707226388e741c2fdb45b179498f588
UasaLine <yury.salagaev@gmail.com>
2021-05-06 15:41:29 +0700

    discount refactor

--- src/main/resources/static/js/shared/discounts-class.js
+++ src/main/resources/static/js/shared/discounts-class.js
@@ -32,4 +32,22 @@ class Discounts {
         const discount = Discounts.findPercent(door);
         return Math.floor(door.priceWithMarkup * discount / 100);
     }
+
+    save(add, close) {
+        var strJSON = JSON.stringify(Discounts.orderDiscountList);
+
+        $.ajax({
+            type: "POST",
+            url: location.origin + "/orderDiscount",
+            contentType: "application/json",
+            data: strJSON,
+            dataType: "json",
+            success: function (data) {
+                Discounts.orderDiscountList = data;
+            },
+            error: function (data) {
+                alert("error: даписать не удалось:(");
+            },
+        });
+    }
 }
