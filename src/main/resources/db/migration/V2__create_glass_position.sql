fbd155fce42d11c0ab82631d4b039e058a7ad3ae
UasaLine <yury.salagaev@gmail.com>
2022-01-27 14:28:56 +0300

    68-DOOR-389 added migrations

---
+++ src/main/resources/db/migration/V2__create_glass_position.sql
@@ -0,0 +1,10 @@
+CREATE TABLE glass_position_entity
+(
+    id     SERIAL PRIMARY KEY,
+    name   CHARACTER VARYING(100),
+    type   CHARACTER VARYING(100),
+    value1 int,
+    value2 int,
+    value3 int,
+    value4 int
+);
